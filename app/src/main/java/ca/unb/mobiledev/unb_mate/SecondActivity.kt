55abbed18a248fb03f3d67778512d75cf46336b3
AndrewDoanTran <dkhanhk1234@gmail.com>
2025-11-19 17:24:20 -0400

    Updated map image + UI changes

--- app/src/main/java/ca/unb/mobiledev/unb_mate/SecondActivity.kt
+++ app/src/main/java/ca/unb/mobiledev/unb_mate/SecondActivity.kt
@@ -37,7 +37,7 @@ class SecondActivity : AppCompatActivity() {
 
         val calendarBtn = findViewById<Button>(R.id.calendarButton)
         val navBtn = findViewById<Button>(R.id.navigationButton)
-        val qrBtn = findViewById<Button>(R.id.qrButton)
+
 
         // Open Calendar
         calendarBtn.setOnClickListener{
@@ -46,17 +46,12 @@ class SecondActivity : AppCompatActivity() {
         }
 
         // Open Google Maps (example: UNB)
-        navBtn.setOnClickListener {
-            val gmmIntentUri = Uri.parse("geo:45.9454,-66.6417?q=University of New Brunswick")
-            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
-            mapIntent.setPackage("com.google.android.apps.maps")
-            startActivity(mapIntent)
-        }
-
-        // Placeholder for QR code scanning
-//        qrBtn.setOnClickListener {
-//            // Later you can integrate ZXing or ML Kit for scanning
-//            welcomeText.text = "QR Scan feature coming soon..."
+//        navBtn.setOnClickListener {
+//            val mapIntent = Intent(this, direction::class.java)
+//            mapIntent.setPackage("com.google.android.apps.maps")
+//            startActivity(mapIntent)
 //        }
+
+
     }
 }
--- app/src/main/java/ca/unb/mobiledev/unb_mate/direction.kt
+++ app/src/main/java/ca/unb/mobiledev/unb_mate/direction.kt
@@ -1,4 +1,12 @@
-package ca.unb.mobiledev.unb_mate
-
-class direction {
-}
+//package ca.unb.mobiledev.unb_mate
+//
+//import androidx.appcompat.app.AppCompatActivity
+//
+//class direction : AppCompatActivity {
+//    @overidefunction onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.)
+//
+//
+//    }
+//}
